2f84213fab59313495120b60248f94114341191a
Yevhenii Molodyi <eugmolody@gmail.com>
2020-11-11 18:16:09 +0200

    Replace CellColor by react hooks implementation

--- js/component/zhn-ch/CellColor.js
+++ js/component/zhn-ch/CellColor.js
@@ -7,69 +7,39 @@ exports["default"] = void 0;
 
 var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
 
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
-
 var _jsxRuntime = require("react/jsx-runtime");
 
-var _react = require("react");
-
 //import PropTypes from 'prop-types'
-var CellColor = /*#__PURE__*/function (_Component) {
-  (0, _inheritsLoose2["default"])(CellColor, _Component);
+var CellColor = function CellColor(_ref) {
+  var _ref$tabIndex = _ref.tabIndex,
+      tabIndex = _ref$tabIndex === void 0 ? '-1' : _ref$tabIndex,
+      style = _ref.style,
+      color = _ref.color,
+      onClick = _ref.onClick,
+      children = _ref.children;
 
-  function CellColor() {
-    var _this;
+  var _styleColor = color ? {
+    backgroundColor: color
+  } : void 0,
+      _onClick = onClick ? onClick.bind(null, color) : void 0;
 
-    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
-    }
-
-    _this = _Component.call.apply(_Component, [this].concat(args)) || this;
-
-    _this._refCellNode = function (node) {
-      return _this.cellNode = node;
-    };
-
-    return _this;
-  }
-
-  var _proto = CellColor.prototype;
-
-  _proto.componentDidMount = function componentDidMount() {
-    var onReg = this.props.onReg;
-
-    if (typeof onReg === 'function') {
-      onReg(this.cellNode);
-    }
-  };
-
-  _proto.render = function render() {
-    var _this$props = this.props,
-        tabIndex = _this$props.tabIndex,
-        style = _this$props.style,
-        color = _this$props.color,
-        onClick = _this$props.onClick,
-        children = _this$props.children,
-        _styleColor = color ? {
-      backgroundColor: color
-    } : undefined,
-        _onClick = onClick ? onClick.bind(null, color) : undefined;
-
-    return /*#__PURE__*/(0, _jsxRuntime.jsx)("button", {
-      ref: this._refCellNode,
-      tabIndex: tabIndex,
-      style: (0, _extends2["default"])({}, style, _styleColor),
-      onClick: _onClick,
-      children: children
-    });
-  };
-
-  return CellColor;
-}(_react.Component);
-
-CellColor.defaultProps = {
-  tabIndex: '-1'
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)("button", {
+    tabIndex: tabIndex,
+    style: (0, _extends2["default"])({}, style, _styleColor),
+    onClick: _onClick,
+    children: children
+  });
 };
+/*
+CellColor.propTypes = {
+  tabIndex: PropTypes.string,
+  style: PropTypes.object,
+  color: PropTypes.string,
+  onClick: PropTypes.func,  
+}
+*/
+
+
 var _default = CellColor;
 exports["default"] = _default;
 //# sourceMappingURL=CellColor.js.map
--- src/component/zhn-ch/CellColor.jsx
+++ src/component/zhn-ch/CellColor.jsx
@@ -1,46 +1,21 @@
-import { Component } from 'react'
 //import PropTypes from 'prop-types'
 
-class CellColor extends Component {
-  /*
-  static propTypes = {
-    tabIndex: PropTypes.string,
-    style: PropTypes.object,
-    color: PropTypes.string,
-    onClick: PropTypes.func,
-    onReg: PropTypes.func
-  }
-  */
-  static defaultProps = {
-    tabIndex: '-1'
-  }
+const CellColor = ({
+  tabIndex='-1',
+  style,
+  color,
+  onClick,
+  children
+}) => {
+    const _styleColor = color
+       ? { backgroundColor: color }
+       : void 0
+    , _onClick = onClick
+       ? onClick.bind(null, color)
+       : void 0;
 
-  componentDidMount(){
-     const { onReg } = this.props;
-     if (typeof onReg === 'function') {
-       onReg(this.cellNode)
-     }
-  }
-
-  _refCellNode = node => this.cellNode = node
-
-  render(){
-    const {
-            tabIndex,
-            style,
-            color,
-            onClick,
-            children
-          } = this.props
-        , _styleColor = color
-             ? { backgroundColor: color }
-             : undefined
-        , _onClick = onClick
-              ? onClick.bind(null, color)
-              : undefined;
     return (
       <button
-         ref={this._refCellNode}
          tabIndex={tabIndex}
          style={{ ...style, ..._styleColor}}
          onClick={_onClick}
@@ -48,8 +23,16 @@ class CellColor extends Component {
          {children}
       </button>
     );
-  }
 }
+
+/*
+CellColor.propTypes = {
+  tabIndex: PropTypes.string,
+  style: PropTypes.object,
+  color: PropTypes.string,
+  onClick: PropTypes.func,  
+}
+*/
 
 
 export default CellColor
