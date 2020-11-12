653dd07113811492227bd10f0666a3e018c03967
Yevhenii Molodyi <eugmolody@gmail.com>
2020-11-12 17:59:47 +0200

    Replace TabDescr by react hooks implementation

--- js/component/d-details/TabDescr.js
+++ js/component/d-details/TabDescr.js
@@ -5,8 +5,6 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 exports.__esModule = true;
 exports["default"] = void 0;
 
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
-
 var _jsxRuntime = require("react/jsx-runtime");
 
 var _react = require("react");
@@ -24,76 +22,45 @@ var CL = {
   ACTIONS: 'md__actions'
 };
 
-var TabDescr = /*#__PURE__*/function (_Component) {
-  (0, _inheritsLoose2["default"])(TabDescr, _Component);
+var TabDescr = function TabDescr(props) {
+  var isSelected = props.isSelected,
+      note = props.note,
+      dispatch = props.dispatch,
+      onClose = props.onClose,
+      id = note.id,
+      _refBtClose = (0, _react.useRef)(null),
+      _refInputDescr = (0, _react.useRef)(null),
+      _editDescr = (0, _react.useCallback)(function () {
+    var _descr = _refInputDescr.current.getValue();
 
-  function TabDescr(props) {
-    var _this;
+    dispatch(_actions["default"].editNoteDescr(id, _descr));
+  }, [id]); //dispatch
 
-    _this = _Component.call(this, props) || this;
 
-    _this._focusBtClose = function () {
-      var _btClose = _this._refBtClose.current;
+  (0, _react.useEffect)(function () {
+    var _btClose = _refBtClose.current;
 
-      if (_this.props.isSelected && _btClose) {
-        _btClose.focus();
-      }
-    };
-
-    _this._editDescr = function () {
-      var _this$props = _this.props,
-          note = _this$props.note,
-          dispatch = _this$props.dispatch,
-          _descr = _this._inputDescr.getValue();
-
-      dispatch(_actions["default"].editNoteDescr(note.id, _descr));
-    };
-
-    _this._refInputDescr = function (node) {
-      return _this._inputDescr = node;
-    };
-
-    _this._refBtClose = /*#__PURE__*/(0, _react.createRef)();
-    return _this;
-  }
-
-  var _proto = TabDescr.prototype;
-
-  _proto.componentDidMount = function componentDidMount() {
-    this._focusBtClose();
-  };
-
-  //_refButtons = (node) => this._buttons = node
-  _proto.render = function render() {
-    var _this$props2 = this.props,
-        note = _this$props2.note,
-        onClose = _this$props2.onClose,
-        _initDescr = (0, _getNoteDescr["default"])(note);
-
-    return /*#__PURE__*/(0, _jsxRuntime.jsxs)(_jsxRuntime.Fragment, {
-      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_InputTextArea["default"], {
-        ref: this._refInputDescr,
-        className: CL.DESCR,
-        maxLength: 250,
-        initValue: _initDescr
-      }, note.id), /*#__PURE__*/(0, _jsxRuntime.jsx)(_DialogButtons["default"] //ref={this._refButtons}
-      , {
-        refBtClose: this._refBtClose,
-        className: CL.ACTIONS,
-        onSave: this._editDescr,
-        onClose: onClose
-      })]
-    });
-  };
-
-  _proto.componentDidUpdate = function componentDidUpdate(prevProps) {
-    if (this.props !== prevProps && this.props.isSelected) {
-      this._focusBtClose();
+    if (isSelected && _btClose) {
+      _btClose.focus();
     }
-  };
+  }, [props]); //isSelected
 
-  return TabDescr;
-}(_react.Component);
+  var _initDescr = (0, _getNoteDescr["default"])(note);
+
+  return /*#__PURE__*/(0, _jsxRuntime.jsxs)(_jsxRuntime.Fragment, {
+    children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_InputTextArea["default"], {
+      ref: _refInputDescr,
+      className: CL.DESCR,
+      maxLength: 250,
+      initValue: _initDescr
+    }, id), /*#__PURE__*/(0, _jsxRuntime.jsx)(_DialogButtons["default"], {
+      refBtClose: _refBtClose,
+      className: CL.ACTIONS,
+      onSave: _editDescr,
+      onClose: onClose
+    })]
+  });
+};
 
 var _default = TabDescr;
 exports["default"] = _default;
--- src/component/d-details/TabDescr.jsx
+++ src/component/d-details/TabDescr.jsx
@@ -1,4 +1,4 @@
-import { createRef, Component } from 'react'
+import { useRef, useCallback, useEffect } from 'react'
 
 import na from '../../flux/note/actions'
 
@@ -12,66 +12,49 @@ const CL = {
   ACTIONS: 'md__actions'
 };
 
-class TabDescr extends Component {
+const TabDescr = (props) => {
+  const {
+    isSelected,
+    note,
+    dispatch,
+    onClose
+  } = props
+  , { id } = note
+  , _refBtClose = useRef(null)
+  , _refInputDescr = useRef(null)
+  , _editDescr = useCallback(() => {
+    const _descr = _refInputDescr.current.getValue();
+    dispatch(na.editNoteDescr(id, _descr))
+  }, [id])
+  //dispatch
 
-  constructor(props){
-    super(props)
-    this._refBtClose = createRef()
-  }
-
-  _focusBtClose = () => {
-    const _btClose = this._refBtClose.current;
-    if (this.props.isSelected && _btClose) {
-      _btClose.focus()      
+  useEffect(() => {
+    const _btClose = _refBtClose.current;
+    if (isSelected && _btClose) {
+      _btClose.focus()
     }
-  }
+  }, [props])
+  //isSelected
 
-  componentDidMount(){
-    this._focusBtClose()
-  }
+  const _initDescr = getNoteDescr(note);
 
-  _editDescr = () => {
-    const { note, dispatch } = this.props
-    , _descr = this._inputDescr.getValue();
-    dispatch(na.editNoteDescr(note.id, _descr))
-  }
-
-  _refInputDescr = (node) => this._inputDescr = node
-  //_refButtons = (node) => this._buttons = node
-
-  render(){
-    const {
-      note,
-      onClose
-    } = this.props
-    , _initDescr = getNoteDescr(note);
-
-    return (
-      <>
-        <InputTextArea
-          ref={this._refInputDescr}
-          key={note.id}
-          className={CL.DESCR}
-          maxLength={250}
-          initValue={_initDescr}
-        />
-        <DialogButtons
-          //ref={this._refButtons}
-          refBtClose={this._refBtClose}
-          className={CL.ACTIONS}
-          onSave={this._editDescr}
-          onClose={onClose}
-        />
-      </>
-    );
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props !== prevProps
-    && this.props.isSelected) {
-      this._focusBtClose()
-    }
-  }
+  return (
+    <>
+      <InputTextArea
+        key={id}
+        ref={_refInputDescr}
+        className={CL.DESCR}
+        maxLength={250}
+        initValue={_initDescr}
+      />
+      <DialogButtons
+        refBtClose={_refBtClose}
+        className={CL.ACTIONS}
+        onSave={_editDescr}
+        onClose={onClose}
+      />
+    </>
+  );
 }
 
 export default TabDescr
