b913f16fc973e1eae4a244abe124f81958f18ce5
Yevhenii Molodyi <eugmolody@gmail.com>
2021-09-20 16:39:06 +0300

    Add crCn to NoteHandle

--- js/component/page-board/NoteHandle.js
+++ js/component/page-board/NoteHandle.js
@@ -9,19 +9,19 @@ var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends")
 
 var _objectWithoutPropertiesLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/objectWithoutPropertiesLoose"));
 
+var _crCn = _interopRequireDefault(require("../zhn-utils/crCn"));
+
 var _jsxRuntime = require("react/jsx-runtime");
 
 var _excluded = ["isDetails"];
-var CL = {
-  HANDLE: 'note__handle',
-  OPEN: 'note__handle--open'
-};
+var CL_HANDLE = 'note__handle',
+    CL_OPEN = 'note__handle--open';
 
 var NoteHandle = function NoteHandle(_ref) {
   var isDetails = _ref.isDetails,
       rest = (0, _objectWithoutPropertiesLoose2["default"])(_ref, _excluded);
 
-  var _cl = isDetails ? CL.HANDLE + " " + CL.OPEN : CL.HANDLE;
+  var _cl = (0, _crCn["default"])(CL_HANDLE, [isDetails, CL_OPEN]);
 
   return /*#__PURE__*/(0, _jsxRuntime.jsx)("span", (0, _extends2["default"])({
     className: _cl
--- src/component/page-board/NoteHandle.jsx
+++ src/component/page-board/NoteHandle.jsx
@@ -1,12 +1,10 @@
-const CL = {
-  HANDLE: 'note__handle',
-  OPEN: 'note__handle--open'
-};
+import crCn from '../zhn-utils/crCn';
+
+const CL_HANDLE = 'note__handle'
+, CL_OPEN = 'note__handle--open';
 
 const NoteHandle = ({ isDetails, ...rest }) => {
-  const _cl = isDetails
-    ? `${CL.HANDLE} ${CL.OPEN}`
-    : CL.HANDLE;
+  const _cl = crCn(CL_HANDLE, [isDetails, CL_OPEN]);
   return (
    <span
       className={_cl}
