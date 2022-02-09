96804890d0e26ea19b0afabd2c64c9e98d766a71
Yevhenii Molodyi <eugmolody@gmail.com>
2022-02-09 12:08:23 +0200

    Add crCn to SvgClose

--- src/component/zhn/SvgClose.jsx
+++ src/component/zhn/SvgClose.jsx
@@ -1,20 +1,25 @@
-const CL_ROOT = "bt-svg-close";
+import crCn from '../zhn-utils/crCn';
+
+const CL_BT_SVG_CLOSE = "bt-svg-close";
 
 const SvgClose = ({
-  className='',
+  className,
   style,
   tabIndex,
   onClose
 }) => (
    <button
       tabIndex={tabIndex}
-      className={`${CL_ROOT} ${className}`}
+      className={crCn(CL_BT_SVG_CLOSE, className)}
       style={style}
       onClick={onClose}
    >
      <svg
-        viewBox="0 0 12 12" width="100%" height="100%"
-        preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 12 12"
+        width="100%"
+        height="100%"
+        preserveAspectRatio="none"
+        xmlns="http://www.w3.org/2000/svg"
       >
         <path d="M 0,0 L 12,12"/>
         <path d="M 12,0 L 0,12"/>
