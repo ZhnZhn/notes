b3ff639dd270a2233bf1f76d6beb525b71d99df5
Yevhenii Molodyi <eugmolody@gmail.com>
2022-06-13 16:49:16 +0300

    Flat page-board components' styles

--- src/component/page-board/NoteCaption.jsx
+++ src/component/page-board/NoteCaption.jsx
@@ -3,11 +3,9 @@ import SvgMore from '../zhn/SvgMore'
 import NoteHandle from './NoteHandle'
 import MenuMore from './NoteMenuMore'
 
-const CL = {
-  CAPTION: 'note__caption',
-  BT_MORE: 'note__bt-more',
-  MENU_MORE: 'note__menu-more'
-};
+const CL_CAPTION = 'note__caption'
+, CL_BT_MORE = 'note__bt-more'
+, CL_MENU_MORE = 'note__menu-more';
 
 const NoteCaption = ({
   dragHandleProps,
@@ -30,19 +28,19 @@ const NoteCaption = ({
       onClick={onClickHandle}
     />
     <InputText
-      className={CL.CAPTION}
+      className={CL_CAPTION}
       value={noteTitle}
       onBlur={onBlurTitle}
     />
     <SvgMore
-      className={CL.BT_MORE}
+      className={CL_BT_MORE}
       title="Click to open note menu"
       onClick={onClickMenuMore}
     />
     {
       isMenuMore && <MenuMore
         isShow={isMenuMore}
-        className={CL.MENU_MORE}
+        className={CL_MENU_MORE}
         onClose={onCloseMenuMore}
         onEdit={onEditDetails}
         onRemove={onRemoveNote}
--- src/component/page-board/NoteMenuMore.jsx
+++ src/component/page-board/NoteMenuMore.jsx
@@ -1,17 +1,16 @@
 import withTheme from '../hoc/withTheme'
 import styleConfig from '../style/Comp.Style'
+import crCn from '../zhn-utils/crCn'
 
 import ModalPane from '../zhn-ch/ModalPane'
 import FlatButton from '../zhn-m/FlatButton'
 import CL from '../style/CL'
 
-const S = {
-  BT: {
-    paddingLeft: 8,
-    width: '100%',
-    textAlign: 'left'
-  }
-}
+const S_BT = {
+  paddingLeft: 8,
+  width: '100%',
+  textAlign: 'left'
+};
 
 const NoteMenuMore = ({
   theme,
@@ -22,21 +21,22 @@ const NoteMenuMore = ({
   onEdit,
   onRemove
 }) => {
-  const TS = theme.createStyle(styleConfig);
+  const TS = theme.createStyle(styleConfig)
+  , _className = crCn(CL.MODAL_PANE, className);
   return (
     <ModalPane
-      className={`${CL.MODAL_PANE} ${className || ''}`}
+      className={_className}
       style={{...style, ...TS.COMP}}
       isShow={isShow}
       onClose={onClose}
     >
         <FlatButton
-          style={S.BT}
+          style={S_BT}
           caption="Edit Details"
           onClick={onEdit}
         />
         <FlatButton
-          style={S.BT}
+          style={S_BT}
           caption="Remove Note"
           onClick={onRemove}
         />
