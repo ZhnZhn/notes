c4e194475a9216dd3cc76a9c308b12d01a292470
Yevhenii Molodyi <eugmolody@gmail.com>
2022-08-23 11:03:42 +0300

    Replace ModalPane class component by function component

--- js/component/zhn-ch/ModalPane.js
+++ js/component/zhn-ch/ModalPane.js
@@ -5,113 +5,33 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 exports.__esModule = true;
 exports["default"] = void 0;
 
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
+var _useClickOutside = _interopRequireDefault(require("../hooks/useClickOutside"));
 
-var _react = require("react");
+var _useKeyEscape = _interopRequireDefault(require("../hooks/useKeyEscape"));
 
 var _jsxRuntime = require("react/jsx-runtime");
 
-var ModalPane = /*#__PURE__*/function (_Component) {
-  (0, _inheritsLoose2["default"])(ModalPane, _Component);
+var ModalPane = function ModalPane(_ref) {
+  var isShow = _ref.isShow,
+      className = _ref.className,
+      style = _ref.style,
+      children = _ref.children,
+      onClose = _ref.onClose;
 
-  function ModalPane() {
-    var _this;
+  var _refNode = (0, _useClickOutside["default"])(isShow, onClose),
+      _hKeyEscape = (0, _useKeyEscape["default"])(onClose),
+      _hKeyDown = isShow ? _hKeyEscape : void 0;
 
-    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
-    }
-
-    _this = _Component.call.apply(_Component, [this].concat(args)) || this;
-
-    _this._isEventTargetOutside = function (event) {
-      return _this.rootNode && _this.rootNode.contains && !_this.rootNode.contains(event.target);
-    };
-
-    _this._hClickOutside = function (event) {
-      var onClose = _this.props.onClose;
-
-      if (_this._isEventTargetOutside(event)) {
-        event.stopPropagation();
-        onClose(event);
-      }
-    };
-
-    _this._addOutsideListener = function () {
-      document.addEventListener('click', _this._hClickOutside, true);
-    };
-
-    _this._removeOutsideListener = function () {
-      document.removeEventListener('click', _this._hClickOutside, true);
-    };
-
-    _this._initShowMode = function () {
-      _this._addOutsideListener();
-
-      if (_this.rootNode) {
-        _this.rootNode.focus();
-      }
-    };
-
-    _this._hKeyDown = function (evt) {
-      if (evt.key === 'Escape') {
-        evt.preventDefault();
-
-        _this.props.onClose(evt);
-      }
-    };
-
-    _this._refRootNode = function (n) {
-      return _this.rootNode = n;
-    };
-
-    return _this;
-  }
-
-  var _proto = ModalPane.prototype;
-
-  _proto.componentDidMount = function componentDidMount() {
-    if (this.props.isShow) {
-      this._initShowMode();
-    }
-  };
-
-  _proto.componentWillUnmount = function componentWillUnmount() {
-    this._removeOutsideListener();
-  };
-
-  _proto.componentDidUpdate = function componentDidUpdate(prevProps) {
-    if (this.props !== prevProps) {
-      if (this.props.isShow) {
-        this._initShowMode();
-      } else {
-        this._removeOutsideListener();
-      }
-    }
-  };
-
-  _proto.render = function render() {
-    var _this$props = this.props,
-        className = _this$props.className,
-        style = _this$props.style,
-        children = _this$props.children;
-    return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
-      ref: this._refRootNode,
-      className: className,
-      style: style,
-      tabIndex: "0" //role="dialog"
-      //aria-modal={true}
-      ,
-      onKeyDown: this._hKeyDown,
-      children: children
-    });
-  };
-
-  return ModalPane;
-}(_react.Component);
-
-ModalPane.defaultProps = {
-  onClose: function onClose() {}
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+    role: "presentation",
+    ref: _refNode,
+    className: className,
+    style: style,
+    onKeyDown: _hKeyDown,
+    children: children
+  });
 };
+
 var _default = ModalPane;
 exports["default"] = _default;
 //# sourceMappingURL=ModalPane.js.map
--- src/component/zhn-ch/ModalPane.jsx
+++ src/component/zhn-ch/ModalPane.jsx
@@ -1,94 +1,29 @@
-import { Component } from '../uiApi'
+import useClickOutside from '../hooks/useClickOutside';
+import useKeyEscape from '../hooks/useKeyEscape';
 
-class ModalPane extends Component {
-  /*
-  static propTypes = {
-    className: PropTypes.string,
-    style: PropTypes.object,
-    isShow: PropTypes.bool,
-    onClose: PropTypes.func
-  }
-  */
-
-  static defaultProps = {
-    onClose: () => {}
-  }
-
-  _isEventTargetOutside = (event) => this.rootNode
-     && this.rootNode.contains
-     && !this.rootNode.contains(event.target)
-
-  _hClickOutside = (event) => {
-    const { onClose } = this.props;
-    if (this._isEventTargetOutside(event)) {
-      event.stopPropagation()
-      onClose(event)
-    }
-  }
-
-  _addOutsideListener = () => {
-    document.addEventListener('click', this._hClickOutside, true)
-  }
-  _removeOutsideListener = () => {
-    document.removeEventListener('click', this._hClickOutside, true)
-  }
-
-  _initShowMode = () => {
-    this._addOutsideListener()
-    if (this.rootNode) {
-      this.rootNode.focus()
-    }
-  }
-
-  componentDidMount() {
-    if (this.props.isShow) {
-      this._initShowMode()
-    }
-  }
-  componentWillUnmount() {
-    this._removeOutsideListener()
-  }
-
-
-  componentDidUpdate(prevProps) {
-    if (this.props !== prevProps ){
-      if (this.props.isShow){
-        this._initShowMode()
-      } else {
-        this._removeOutsideListener()
-      }
-    }
-  }
-
-  _hKeyDown = (evt) => {
-    if (evt.key === 'Escape') {
-      evt.preventDefault()
-      this.props.onClose(evt)
-    }
-  }
-
-  _refRootNode = n => this.rootNode = n
-
-  render(){
-    const {
-      className,
-      style,
-      children
-    } = this.props;
-    return (
-      <div
-        ref={this._refRootNode}
-        className={className}
-        style={style}
-        tabIndex="0"
-        //role="dialog"
-        //aria-modal={true}
-        onKeyDown={this._hKeyDown}
-      >
-        {children}
-      </div>
-    );
-  }
+const ModalPane = ({
+  isShow,
+  className,
+  style,
+  children,
+  onClose
+}) => {
+  const _refNode = useClickOutside(isShow, onClose)
+  , _hKeyEscape = useKeyEscape(onClose)
+  , _hKeyDown = isShow
+       ? _hKeyEscape
+       : void 0;
+  return (
+    <div
+       role="presentation"
+       ref={_refNode}
+       className={className}
+       style={style}
+       onKeyDown={_hKeyDown}
+    >
+      {children}
+    </div>
+  );
 }
 
 export default ModalPane
