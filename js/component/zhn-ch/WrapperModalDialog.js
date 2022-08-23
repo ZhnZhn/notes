791010083f5e58984fbe130fad8ed45ac2d96345
Yevhenii Molodyi <eugmolody@gmail.com>
2022-08-23 12:27:58 +0300

    Replace WrapperModalDialog class component by function component

--- js/component/zhn-ch/WrapperModalDialog.js
+++ js/component/zhn-ch/WrapperModalDialog.js
@@ -1,109 +1,37 @@
 "use strict";
 
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
 exports.__esModule = true;
 exports["default"] = void 0;
 
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
-
-var _react = require("react");
-
 var _jsxRuntime = require("react/jsx-runtime");
 
-//import PropTypes from 'prop-types'
-var CL = {
-  INIT: 'modal-root',
-  SHOWING: 'modal-root show-modal',
-  HIDING: 'modal-root hide-modal'
-};
-var S = {
-  SHOW: {
-    display: 'block'
-  },
-  HIDE: {
-    display: 'none'
-  },
-  HIDE_BACKGROUND: {
-    backgroundColor: 'rgba(0,0,0, 0)'
-  }
+var CL_INIT = 'modal-root',
+    CL_SHOWING = CL_INIT + " show-modal",
+    S_SHOW = {
+  display: 'block'
+},
+    S_HIDE = {
+  display: 'none'
 };
 
-var WrapperModalDialog = /*#__PURE__*/function (_Component) {
-  (0, _inheritsLoose2["default"])(WrapperModalDialog, _Component);
+var WrapperModalDialog = function WrapperModalDialog(_ref) {
+  var isShow = _ref.isShow,
+      onClose = _ref.onClose,
+      children = _ref.children;
 
-  /*
-  static propTypes = {
-    isShow: PropTypes.bool,
-    timeout: PropTypes.number,
-    onClose: PropTypes.func
-  }
-  */
-  function WrapperModalDialog(props) {
-    var _this;
+  var _ref2 = isShow ? [CL_SHOWING, S_SHOW] : [CL_INIT, S_HIDE],
+      _className = _ref2[0],
+      _style = _ref2[1];
 
-    _this = _Component.call(this) || this;
-
-    _this._hideModal = function () {
-      _this.wasClosing = true;
-
-      _this.setState({});
-    };
-
-    _this.wasClosing = true;
-    return _this;
-  }
-
-  var _proto = WrapperModalDialog.prototype;
-
-  _proto.shouldComponentUpdate = function shouldComponentUpdate(nextProps, nextState) {
-    if (!this.wasClosing && !nextProps.isShow && !this.props.isShow) {
-      return false;
-    }
-
-    return true;
-  };
-
-  _proto.componentDidUpdate = function componentDidUpdate(prevProps, prevState) {
-    if (prevProps.isShow && !this.props.isShow) {
-      setTimeout(this._hideModal, this.props.timeout);
-    }
-  };
-
-  _proto.render = function render() {
-    var _this$props = this.props,
-        isShow = _this$props.isShow,
-        children = _this$props.children,
-        onClose = _this$props.onClose;
-
-    var _className, _style, _isHidden;
-
-    if (!isShow && this.wasClosing) {
-      _className = CL.INIT;
-      _style = S.HIDE;
-      _isHidden = true;
-      this.wasClosing = false;
-    } else {
-      _className = isShow ? CL.SHOWING : CL.HIDING;
-      _style = isShow ? S.SHOW : S.HIDE_BACKGROUND;
-      _isHidden = false;
-    }
-
-    return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
-      className: _className,
-      style: _style,
-      "aria-hidden": _isHidden,
-      onClick: onClose,
-      children: children
-    });
-  };
-
-  return WrapperModalDialog;
-}(_react.Component);
-
-WrapperModalDialog.defaultProps = {
-  timeout: 450
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+    role: "presentation",
+    className: _className,
+    style: _style,
+    onClick: onClose,
+    children: children
+  });
 };
+
 var _default = WrapperModalDialog;
 exports["default"] = _default;
 //# sourceMappingURL=WrapperModalDialog.js.map
--- src/component/zhn-ch/WrapperModalDialog.jsx
+++ src/component/zhn-ch/WrapperModalDialog.jsx
@@ -1,89 +1,30 @@
-import { Component } from '../uiApi';
-
-import crCn from '../zhn-utils/crCn'
-//import PropTypes from 'prop-types'
-
-const CL_MODAL_ROOT = 'modal-root'
-, CL_SHOW_MODAL = 'show-modal'
-, CL_HIDE_MODAL = 'hide-modal'
-
+const CL_INIT = 'modal-root'
+, CL_SHOWING = `${CL_INIT} show-modal`
 , S_SHOW = { display: 'block' }
-, S_HIDE = { display: 'none' }
-, S_HIDE_BACKGROUND = { backgroundColor: 'rgba(0,0,0, 0)' };
+, S_HIDE = { display: 'none' };
 
-class WrapperModalDialog extends Component {
-  /*
-  static propTypes = {
-    isShow: PropTypes.bool,
-    timeout: PropTypes.number,
-    onClose: PropTypes.func
-  }
-  */
-  static defaultProps = {
-    timeout: 450
-  }
+const WrapperModalDialog = ({
+  isShow,
+  onClose,
+  children
+}) => {
+  const [
+    _className,
+    _style
+  ] = isShow
+    ? [CL_SHOWING, S_SHOW]
+    : [CL_INIT, S_HIDE];
 
-  wasClosing = true
-
-
-  shouldComponentUpdate(nextProps, nextState){
-    if (!this.wasClosing
-      && !nextProps.isShow
-      && !this.props.isShow) {
-      return false;
-    }
-    return true;
-  }
-
-  componentDidUpdate(prevProps, prevState){
-    if (prevProps.isShow && !this.props.isShow) {
-      setTimeout(
-        this._hideModal,
-        this.props.timeout
-      )
-    }
-  }
-
-  _hideModal = () => {
-    this.wasClosing = true
-    this.setState({})
-  }
-
-  render(){
-    const {
-      isShow,
-      children,
-      onClose
-    } = this.props;
-    let _className, _style, _isHidden;
-    if (!isShow && this.wasClosing){
-       _className = CL_MODAL_ROOT;
-       _style = S_HIDE;
-       _isHidden = true;
-       this.wasClosing = false;
-    } else {
-      _className = crCn(
-        CL_MODAL_ROOT,
-        isShow ? CL_SHOW_MODAL : CL_HIDE_MODAL
-      )
-      _style = isShow
-          ? S_SHOW
-          : S_HIDE_BACKGROUND;
-      _isHidden = false
-    }
-
-    return (
-      <div
-        role="presentation"
-        className={_className}
-        style={_style}
-        aria-hidden={_isHidden}
-        onClick={onClose}
-      >
-        {children}
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      role="presentation"
+      className={_className}
+      style={_style}
+      onClick={onClose}
+    >
+      {children}
+    </div>
+  );
+};
 
 export default WrapperModalDialog
