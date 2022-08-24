3631b58a24a204acf26091436a41dcf3f8a4c836
Yevhenii Molodyi <eugmolody@gmail.com>
2022-08-24 12:25:51 +0300

    Edit SettingsDialog

--- js/component/dialogs/SettingsDialog.js
+++ js/component/dialogs/SettingsDialog.js
@@ -5,17 +5,15 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 exports.__esModule = true;
 exports["default"] = void 0;
 
-var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
+var _uiApi = require("../uiApi");
 
-var _react = require("react");
+var _memoIsShow = _interopRequireDefault(require("../hoc/memoIsShow"));
 
 var _useTheme = _interopRequireDefault(require("../hooks/useTheme"));
 
 var _Dialog = _interopRequireDefault(require("../style/Dialog.Style"));
 
 var _reducer = require("../../flux/app/reducer");
-
-var _selectors = require("../../flux/selectors");
 
 var _ModalDialog = _interopRequireDefault(require("../zhn-ch/ModalDialog"));
 
@@ -24,10 +22,6 @@ var _InputSelect = _interopRequireDefault(require("../zhn-m-input/InputSelect"))
 var _jsxRuntime = require("react/jsx-runtime");
 
 var CL = 'md-settings';
-var DF_THEME = {
-  caption: "Dark",
-  value: "DARK"
-};
 var _themeOptions = [{
   caption: "Dark",
   value: "DARK"
@@ -37,70 +31,40 @@ var _themeOptions = [{
 }, {
   caption: "Sand",
   value: "SAND"
-}];
-
-var _crInitItem = function _crInitItem(uiTheme) {
-  if (!uiTheme) {
-    return DF_THEME;
-  }
-
-  var item = _themeOptions.find(function (item) {
-    return item.value === uiTheme;
-  });
-
-  if (!item) {
-    return DF_THEME;
-  }
-
-  return (0, _extends2["default"])({}, item);
-};
-
-var SettingsDialog = function SettingsDialog(_ref) {
+}],
+    DF_THEME = _themeOptions[0];
+var SettingsDialog = (0, _memoIsShow["default"])(function (_ref) {
   var isShow = _ref.isShow,
-      store = _ref.store,
       dispatch = _ref.dispatch,
       onClose = _ref.onClose;
 
-  var _useState = (0, _react.useState)(function () {
-    return _crInitItem(_selectors.sApp.uiTheme(store.getState()));
-  }),
-      initItem = _useState[0],
-      setInitItem = _useState[1],
-      TS = (0, _useTheme["default"])(_Dialog["default"]),
-      _selectTheme = (0, _react.useCallback)(function (_ref2) {
+  /*eslint-disable react-hooks/exhaustive-deps */
+  var _selectTheme = (0, _uiApi.useCallback)(function (_ref2) {
     var value = _ref2.value;
+    dispatch((0, _reducer.setUiTheme)({
+      uiTheme: value
+    }));
+  }, []) // dispatch
 
-    if (value !== initItem) {
-      dispatch((0, _reducer.setUiTheme)({
-        uiTheme: value
-      }));
-      setInitItem(value);
-    }
-  }, []);
+  /*eslint-enable react-hooks/exhaustive-deps */
+  ,
+      TS = (0, _useTheme["default"])(_Dialog["default"]);
 
   return /*#__PURE__*/(0, _jsxRuntime.jsx)(_ModalDialog["default"], {
+    isShow: isShow,
     className: CL,
     style: TS.DIALOG,
     caption: "User Settings",
-    isShow: isShow,
     onClose: onClose,
-    children: /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
-      children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_InputSelect["default"], {
-        styleConfig: TS.SELECT,
-        caption: "UI Theme (Default: Dark)",
-        initItem: initItem,
-        options: _themeOptions,
-        onSelect: _selectTheme
-      })
+    children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_InputSelect["default"], {
+      styleConfig: TS.SELECT,
+      caption: "UI Theme (Default: Dark)",
+      initialItem: DF_THEME,
+      options: _themeOptions,
+      onSelect: _selectTheme
     })
   });
-};
-
-var _areEqualProps = function _areEqualProps(prevProps, nextProps) {
-  return prevProps.isShow === nextProps.isShow;
-};
-
-var _default = /*#__PURE__*/(0, _react.memo)(SettingsDialog, _areEqualProps);
-
+});
+var _default = SettingsDialog;
 exports["default"] = _default;
 //# sourceMappingURL=SettingsDialog.js.map
--- src/component/dialogs/SettingsDialog.jsx
+++ src/component/dialogs/SettingsDialog.jsx
@@ -1,72 +1,53 @@
 import {
-  useState,
   useCallback
-} from '../uiApi'
+} from '../uiApi';
 
-import memoIsShow from '../hoc/memoIsShow'
+import memoIsShow from '../hoc/memoIsShow';
 
-import useTheme from '../hooks/useTheme'
-import styleConfig from '../style/Dialog.Style'
-import { setUiTheme } from '../../flux/app/reducer'
-import { sApp } from '../../flux/selectors'
+import useTheme from '../hooks/useTheme';
+import styleConfig from '../style/Dialog.Style';
+import { setUiTheme } from '../../flux/app/reducer';
 
-import ModalDialog from '../zhn-ch/ModalDialog'
-import InputSelect from '../zhn-m-input/InputSelect'
+import ModalDialog from '../zhn-ch/ModalDialog';
+import InputSelect from '../zhn-m-input/InputSelect';
 
 const CL = 'md-settings';
 
-const DF_THEME = { caption: "Dark", value: "DARK" };
 const _themeOptions = [
   { caption: "Dark", value: "DARK" },
   { caption: "Light", value: "WHITE" },
   { caption: "Sand", value: "SAND" }
-];
-
-const _crInitItem = (uiTheme) => {
-  if (!uiTheme) {
-    return DF_THEME;
-  }
-  const item = _themeOptions
-    .find(item => item.value === uiTheme);
-  if (!item) {
-    return DF_THEME;
-  }
-  return { ...item };
-}
+]
+, DF_THEME = _themeOptions[0];
 
 const SettingsDialog = memoIsShow(({
   isShow,
-  store,
   dispatch,
   onClose
 }) => {
-
-  const [initItem, setInitItem] = useState(() => _crInitItem(sApp.uiTheme(store.getState())))
-  , TS = useTheme(styleConfig)
-  , _selectTheme = useCallback(({ value }) => {
-    if (value !== initItem) {
+  /*eslint-disable react-hooks/exhaustive-deps */
+  const _selectTheme = useCallback(({ value }) => {
       dispatch(setUiTheme({ uiTheme: value }))
-      setInitItem(value)
-    }
-  }, []);
+  }, [])
+  // dispatch
+  /*eslint-enable react-hooks/exhaustive-deps */
+  , TS = useTheme(styleConfig);
 
   return (
     <ModalDialog
+      isShow={isShow}
       className={CL}
       style={TS.DIALOG}
       caption="User Settings"
-      isShow={isShow}
       onClose={onClose}
     >
-      <div>
-        <InputSelect
-          styleConfig={TS.SELECT}
-          caption="UI Theme (Default: Dark)"
-          initItem={initItem}
-          options={_themeOptions}
-          onSelect={_selectTheme}
-        />
-      </div>
+      <InputSelect
+        styleConfig={TS.SELECT}
+        caption="UI Theme (Default: Dark)"
+        initialItem={DF_THEME}
+        options={_themeOptions}
+        onSelect={_selectTheme}
+      />
     </ModalDialog>
   );
 })
