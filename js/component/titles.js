86cf77f8dc616d3466439422c06706991544c35e
Yevhenii Molodyi <eugmolody@gmail.com>
2022-08-25 10:34:53 +0300

    Release of web app Notes v0.4.0

--- js/component/titles.js
+++ js/component/titles.js
@@ -2,6 +2,6 @@
 
 exports.__esModule = true;
 exports.APP_TITLE = void 0;
-var APP_TITLE = "Web App Notes v0.3.0";
+var APP_TITLE = "Web App Notes v0.4.0";
 exports.APP_TITLE = APP_TITLE;
 //# sourceMappingURL=titles.js.map
--- src/component/titles.js
+++ src/component/titles.js
@@ -1,1 +1,1 @@
-export const APP_TITLE = "Web App Notes v0.3.0"
+export const APP_TITLE = "Web App Notes v0.4.0"
