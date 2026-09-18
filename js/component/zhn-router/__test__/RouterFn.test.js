40814a7c1704dfbc260ac417d104ecfa681cdb66
Yevhenii Molodyi <eugmolody@gmail.com>
2026-09-18 16:33:52 +0300

    Add tests for function parsePath named export from RouterFn module

--- js/component/zhn-router/__test__/RouterFn.test.js
+++ js/component/zhn-router/__test__/RouterFn.test.js
@@ -53,4 +53,106 @@ describe("normalizeLeadingSlash", () => {
     expect(fn(null)).toBe("/");
   });
 });
+describe("parsePath", () => {
+  const fn = _RouterFn.parsePath;
+  test("should parse a simple pathname without special characters", () => {
+    expect(fn("/items")).toEqual({
+      pathname: "/items"
+    });
+  });
+  test("should parse a complex pathname with special characters", () => {
+    expect(fn("/items/props/about")).toEqual({
+      pathname: "/items/props/about"
+    });
+    expect(fn("/a-b_name/sub-path")).toEqual({
+      pathname: "/a-b_name/sub-path"
+    });
+    expect(fn("/items/props/123")).toEqual({
+      pathname: "/items/props/123"
+    });
+  });
+  test("should parse search parameters", () => {
+    expect(fn("/path?key=value")).toEqual({
+      pathname: "/path",
+      search: "?key=value"
+    });
+    expect(fn("/items?id=1&available=true")).toEqual({
+      pathname: "/items",
+      search: "?id=1&available=true"
+    });
+    expect(fn("/search?q=some%20token")).toEqual({
+      pathname: "/search",
+      search: "?q=some%20token"
+    });
+    expect(fn("?key=value")).toEqual({
+      search: "?key=value"
+    });
+    expect(fn("/items?")).toEqual({
+      pathname: "/items",
+      search: "?"
+    });
+  });
+  test("should parse hash fragments", () => {
+    expect(fn("/page#a-section_2")).toEqual({
+      pathname: "/page",
+      hash: "#a-section_2"
+    });
+    expect(fn("#top")).toEqual({
+      hash: "#top"
+    });
+    expect(fn("/page#")).toEqual({
+      pathname: "/page",
+      hash: "#"
+    });
+  });
+  test("should parse pathname with search and hash", () => {
+    expect(fn("/items?id=1#review")).toEqual({
+      pathname: "/items",
+      search: "?id=1",
+      hash: "#review"
+    });
+    expect(fn("/search?q=app%20router&filter=docs#getting-started")).toEqual({
+      pathname: "/search",
+      search: "?q=app%20router&filter=docs",
+      hash: "#getting-started"
+    });
+  });
+  test("should parse in complex and edge cases", () => {
+    expect(fn("/")).toEqual({
+      pathname: "/"
+    });
+    expect(fn("/items/")).toEqual({
+      pathname: "/items/"
+    });
+    expect(fn("?key=value#hash")).toEqual({
+      search: "?key=value",
+      hash: "#hash"
+    });
+    expect(fn("/page#section#subsection")).toEqual({
+      pathname: "/page",
+      hash: "#section#subsection"
+    });
+    expect(fn("/path?key=value?other")).toEqual({
+      pathname: "/path",
+      search: "?key=value?other"
+    });
+    expect(fn("/api/v1.0/data")).toEqual({
+      pathname: "/api/v1.0/data"
+    });
+    expect(fn("/search?q=some token 2")).toEqual({
+      pathname: "/search",
+      search: "?q=some token 2"
+    });
+    expect(fn("/items?query#hash")).toEqual({
+      pathname: "/items",
+      search: "?query",
+      hash: "#hash"
+    });
+  });
+  test("should return an empty object in edge cases inputs", () => {
+    expect(fn("")).toEqual({});
+    expect(fn(null)).toEqual({});
+    expect(fn()).toEqual({});
+  });
+});
 //# sourceMappingURL=RouterFn.test.js.map
